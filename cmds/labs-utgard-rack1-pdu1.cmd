f202a38c6907c83663a884533a7bb08dd95dd630
Wayne Lewis <wayne.lewis@esss.se>
2018-08-21 00:27:51 +0200

    Modularise startup script

--- cmds/labs-utgard-rack1-pdu1.cmd
+++ cmds/labs-utgard-rack1-pdu1.cmd
@@ -11,13 +11,11 @@ epicsEnvSet(ENGINEER, "Wayne Lewis")
 
 ## Paths
 epicsEnvSet(TOP, "$(E3_CMD_TOP)")
-epicsEnvSet("DB_TOP", "$(TOP)/../template/")
 epicsEnvSet("AUTOSAVE_DIR", "/opt/nonvolatile_local")
-epicsEnvSet("MIBDIRS", "+$(TOP)/../mibs")
-epicsEnvSet("IOCSTATS_CMD_TOP", "$(TOP)/../e3-iocStats/cmds")
-epicsEnvSet("AUTOSAVE_CMD_TOP", "$(TOP)/../e3-autosave/cmds")
-epicsEnvSet("SNMP_CMD_TOP", "$(TOP)/../e3-snmp/cmds")
-epicsEnvSet("RECSYNC_CMD_TOP", "$(TOP)/../e3-snmp/cmds")
+epicsEnvSet("IOCSTATS_CMD_TOP", "$(TOP)/../../e3-iocStats/cmds")
+epicsEnvSet("AUTOSAVE_CMD_TOP", "$(TOP)/../../e3-autosave/cmds")
+epicsEnvSet("SNMP_CMD_TOP", "$(TOP)/../../e3-snmp/cmds")
+epicsEnvSet("RECSYNC_CMD_TOP", "$(TOP)/../../e3-recsync/cmds")
 
 ## Database macros
 #epicsEnvSet(SYS, "LabS-Utgard-VIP:")
@@ -34,13 +32,13 @@ iocshLoad "$(SNMP_CMD_TOP)/raritan-px3-5190r.cmd" "PDU_IP=$(PDU_IP),IOC=$(IOC)"
 
 #devSnmpSetParam("DebugLevel",100)
 
-iocshLoad "$(AUTOSAVE_CMD_TOP)/save_restore_before_init.cmd" "P=$(P),R=$(R), IOC=$(IOC), AS_TOP=$(TOP)"
+iocshLoad "$(AUTOSAVE_CMD_TOP)/save_restore_before_init.cmd" "P=$(P),R=$(R), IOC=$(IOC), AS_TOP=/opt/nonvolatile_local/$(IOC)"
 
 iocshLoad "$(RECSYNC_CMD_TOP)/recsync.cmd"
 
 iocInit()
 
-iocshLoad "$(AUTOSAVE_CMD_TOP)/save_restore_after_init.cmd" "P=$(P),R=$(R), IOC=$(IOC), AS_TOP=$(TOP)"
+iocshLoad "$(AUTOSAVE_CMD_TOP)/save_restore_after_init.cmd" "P=$(P),R=$(R), IOC=$(IOC), AS_TOP=/opt/nonvolatile_local/$(IOC)"
 
 dbl > "$(TOP)/$(IOC)_PVs.list"
 
