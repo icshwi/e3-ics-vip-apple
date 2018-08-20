426889ef7cf1c825568867f7b5d0eb24e3d8e22b
Wayne Lewis <wayne.lewis@esss.se>
2018-08-20 22:17:22 +0200

    Create startup script for chopper rack PDU

--- cmds/labs-utgard-rack1-pdu1.cmd
+++ cmds/labs-utgard-rack1-pdu1.cmd
@@ -1,59 +1,46 @@
-## cmds
+# Required modules
+## module_name,version
 
 require snmp,1.0.0.1
 require iocStats,1856ef5
 require autosave,5.9.0
 require recsync,1.3.0
 
-epicsEnvSet(TOP, "$(E3_CMD_TOP)")
+# Environment variables
+epicsEnvSet(ENGINEER, "Wayne Lewis")
 
+## Paths
+epicsEnvSet(TOP, "$(E3_CMD_TOP)")
+epicsEnvSet("DB_TOP", "$(TOP)/../template/")
+epicsEnvSet("AUTOSAVE_DIR", "/opt/nonvolatile_local")
+epicsEnvSet("MIBDIRS", "+$(TOP)/../mibs")
+epicsEnvSet("IOCSTATS_CMD_TOP", "$(TOP)/../e3-iocStats/cmds")
+epicsEnvSet("AUTOSAVE_CMD_TOP", "$(TOP)/../e3-autosave/cmds")
+epicsEnvSet("SNMP_CMD_TOP", "$(TOP)/../e3-snmp/cmds")
+epicsEnvSet("RECSYNC_CMD_TOP", "$(TOP)/../e3-snmp/cmds")
+
+## Database macros
+#epicsEnvSet(SYS, "LabS-Utgard-VIP:")
+#epicsEnvSet(DEV, "Ctrl-PDU-Rack1")
 epicsEnvSet(P, "LabS-Utgard-VIP:Rack-Chopper")
 epicsEnvSet(R, "PDU")
 epicsEnvSet("IOC",  "$(P)-$(R)")
-epicsEnvSet("IOCST", "$(IOC):IocStats")
+#epicsEnvSet("IOC",  "$(SYS)$(DEV)")
+epicsEnvSet("PDU_IP", "10.4.0.121")
 
-epicsEnvSet("AUTOSAVE_DIR", "/opt/nonvolatile_local")
+iocshLoad "$(IOCSTATS_CMD_TOP)/iocStats.cmd" "IOC=$(IOC):IocStats")
 
-dbLoadRecords("iocAdminSoft.db","IOC=${IOCST}")
-
-epicsEnvSet("MIBDIRS", "+$(TOP)/../mibs")
-epicsEnvSet("DB_TOP", "$(TOP)/../template/")
-
-devSnmpSetSnmpVersion("10.4.0.121","SNMP_VERSION_2c")
-
-# MIB file Prefix
-epicsEnvSet("PDU1", "10.4.0.121")
-
-dbLoadTemplate("$(DB_TOP)/raritan-PX3-5190R-ess.substitutions", "PREFIX=$(IOC):, PDU_IP=$(PDU1)")
+iocshLoad "$(SNMP_CMD_TOP)/raritan-px3-5190r.cmd" "PDU_IP=$(PDU_IP),IOC=$(IOC)"
 
 #devSnmpSetParam("DebugLevel",100)
 
-## autosave/restore machinery
-save_restoreSet_Debug(0)                        # optional
-save_restoreSet_IncompleteSetsOk(1)             # optional
-save_restoreSet_DatedBackupFiles(1)             # optional
- 
-system("install -m 777 -d $(AUTOSAVE_DIR)/$(IOC)/autosave/save")# optional, but if omitted, must ensure path exists, requires system module
-system("install -m 777 -d $(AUTOSAVE_DIR)/$(IOC)/autosave/req") # optional, but if omitted, must ensure path exists, requires system module
- 
-set_savefile_path("$(AUTOSAVE_DIR)/$(IOC)/autosave","/save")    # optional, but recommended. Replace TOP macro as required
-set_requestfile_path("$(AUTOSAVE_DIR)/$(IOC)/autosave","/req")  # optional, but recommended. Replace TOP macro as required
- 
-set_pass0_restoreFile("info_positions.sav")     # before record initialization
-set_pass0_restoreFile("info_settings.sav")      # before record initialization
-set_pass1_restoreFile("info_settings.sav")      # after record initialization
+iocshLoad "$(AUTOSAVE_CMD_TOP)/save_restore_before_init.cmd" "P=$(P),R=$(R), IOC=$(IOC), AS_TOP=$(TOP)"
 
-# Set recsync parameters
-var(reccastTimeout, 5.0)
-var(reccastMaxHoldoff, 5.0)
+iocshLoad "$(RECSYNC_CMD_TOP)/recsync.cmd"
 
 iocInit()
 
-## more autosave/restore machinery
-cd $(AUTOSAVE_DIR)/$(IOC)/autosave/req          # sets location for autosave file generation
-makeAutosaveFiles()                             # generates info_positions.req and info_settings.req from info tags in database
-create_monitor_set("info_positions.req", 5 , "")
-create_monitor_set("info_settings.req", 15 , "")
+iocshLoad "$(AUTOSAVE_CMD_TOP)/save_restore_after_init.cmd" "P=$(P),R=$(R), IOC=$(IOC), AS_TOP=$(TOP)"
 
 dbl > "$(TOP)/$(IOC)_PVs.list"
 
