602df045b96ae804f5c57cc5f5f44d11b7039720
Wayne Lewis <wayne.lewis@esss.se>
2018-08-31 23:41:25 +0200

    Remove allow setting from PDU startup script

--- cmds/start_bg_labs-utgard-rack1-pdu1.cmd.sh
+++ cmds/start_bg_labs-utgard-rack1-pdu1.cmd.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 source /epics/base-3.15.5/require/3.0.0/bin/setE3Env.bash
-nohup iocsh.bash labs-utgard-rack1-pdu1.cmd < /dev/zero &
+#nohup iocsh.bash labs-utgard-rack1-pdu1.cmd < /dev/zero &
+/usr/bin/procServ -f -L /var/log/procServ/labs-utgard-rack1-pdu1 -i ^C^D -c /var/run/procServ/labs-utgard-rack1-pdu1 2000 /epics/base-3.15.5/require/3.0.0/bin/iocsh.bash /home/ioc/iocs/ics-vip-apple/cmds/labs-utgard-rack1-pdu1.cmd &
